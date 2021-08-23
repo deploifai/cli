bbf185218f60839590069818704dc2ae584d46a3
seanchok <seanchok@gmail.com>
2021-08-24 02:44:22 +0800

    install requests pip package

--- setup.py
+++ setup.py
@@ -7,9 +7,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     python_requires=">=3.6",
-    install_requires=[
-        "Click",
-    ],
+    install_requires=["click", "keyring==23.0.1", "requests"],
     entry_points={
         "console_scripts": [
             "deploifai = deploifai.cli:cli",
