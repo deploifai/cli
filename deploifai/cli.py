5669f3ad487e51d42628d5b18e03449850c6ea54
seanchok <seanchok@gmail.com>
2021-08-24 17:28:21 +0800

    fix version escape callback unable to read the correct package

--- deploifai/cli.py
+++ deploifai/cli.py
@@ -7,7 +7,7 @@ from .auth.logout import logout
 
 
 @click.group()
-@click.version_option(message="%(prog)s %(version)s")
+@click.version_option(package_name="deploifai-cli", message="%(prog)s %(version)s")
 @pass_deploifai_context_obj
 @click.option("--debug", is_flag=True, help="Show debug logs")
 @click.option(
