57a07d9c25b54d584984d7f396bceb91a2f00ec3
seanchok <seanchok@gmail.com>
2021-08-24 17:29:56 +0800

    install black formatter

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open('readme.md', 'r', encoding='utf-8') as fh:
+with open("readme.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -10,14 +10,14 @@ setuptools.setup(
     description="Deploifai CLI",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/deploifai/cli',
+    url="https://github.com/deploifai/cli",
     packages=setuptools.find_packages(),
     include_package_data=True,
     python_requires=">=3.9",
     install_requires=["click", "keyring", "requests"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent"
+        "Operating System :: OS Independent",
     ],
     entry_points={
         "console_scripts": [
