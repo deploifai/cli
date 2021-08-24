1d1c9782904bbd21e14a3ec1efb3a84e02d0f0c5
seanchok <seanchok@gmail.com>
2021-08-24 16:48:12 +0800

    setup package distribution dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 import setuptools
 
+with open('readme.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setuptools.setup(
-    name="deploifai",
+    name="deploifai-cli",
     version="0.0.1",
     author="Deploifai Limited",
+    description="Deploifai CLI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/deploifai/cli',
     packages=setuptools.find_packages(),
     include_package_data=True,
     python_requires=">=3.9",
     install_requires=["click", "keyring", "requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
     entry_points={
         "console_scripts": [
             "deploifai = deploifai.cli:cli",
