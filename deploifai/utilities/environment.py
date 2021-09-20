d53923af987e270279492040e855857f939ae2fc
Sean Chok <seanchok@gmail.com>
2021-09-20 17:09:31 +0800

    add python-dotenv to load env (#3)

--- deploifai/utilities/environment.py
+++ deploifai/utilities/environment.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 cli_environment = os.environ.get("DEPLOIFAI_CLI_ENVIRONMENT", "production")
 is_development_env = cli_environment == "development"
 
 backend_url = os.environ.get("DEPLOIFAI_BACKEND_URL", "https://api.deploif.ai")
+
