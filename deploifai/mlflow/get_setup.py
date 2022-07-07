2352c30d4c99a7a6daca0ba27909ae4523fee2e1
KSChauhan2 <kunalsinghchauhan@gmail.com>
2022-07-07 18:57:55 +0800

    Used decorator instead of if condition

--- deploifai/mlflow/get_setup.py
+++ deploifai/mlflow/get_setup.py
@@ -3,7 +3,7 @@ import pyperclip
 from enum import Enum
 
 from deploifai.api import DeploifaiAPIError
-from deploifai.context import pass_deploifai_context_obj, DeploifaiContextObj, is_authenticated
+from deploifai.context import pass_deploifai_context_obj, DeploifaiContextObj, is_authenticated, project_found
 from PyInquirer import prompt
 
 
@@ -17,102 +17,82 @@ class Environment(Enum):
 @click.option("--external", is_flag=True, help="Use mlflow integration outside of Deploifai managed runner")
 @pass_deploifai_context_obj
 @is_authenticated
+@project_found
 def get_setup(context: DeploifaiContextObj, external):
     """
     Get setup to integrate mlflow in training scripts
     """
 
     # assume that the user should be in a project directory, that contains local configuration file
-    if 'id' in context.local_config['PROJECT']:
-        project_id = context.local_config['PROJECT']['id']
-        # getting all the required information to provide the code
-        fragment = """
-                    fragment project on Project {
-                        name
-                        account{
-                            username
-                        }
-                        experiments{
-                          name
-                          environment
-                          resourceAccessTokenString
-                        } 
+    project_id = context.local_config['PROJECT']['id']
+    # getting all the required information to provide the code
+    fragment = """
+                fragment project on Project {
+                    name
+                    account{
+                        username
                     }
-                    """
-        context.debug_msg(project_id)
-        project_data = context.api.get_project(project_id=project_id, fragment=fragment)
-        project_name = project_data["name"]
-        workspace_name = project_data["account"]["username"]
-        experiment_data = project_data["experiments"]
-
-        """
-        experiment_names = [experiment["name"] for experiment in experiment_data]
-        experiment_environments = [experiment["environment"] for experiment in experiment_data]
-        experiment_tokens = [experiment["resourceAccessTokenString"] for experiment in experiment_data]
-        """
-
-        click.secho("Workspace: {}".format(workspace_name), fg='blue')
-        click.secho("Project: {}<{}>".format(project_name, project_id), fg='blue')
-
-        # selecting experiment name from the options
-        if len(experiment_data) == 0:
-            click.secho("No experiments exist for the project", fg='yellow')
-            return
-        else:
-            choose_experiment = prompt(
-                {
-                    "type": "list",
-                    "name": "experiment",
-                    "message": "Choose a experiment",
-                    "choices": [
-                        {
-                            "name": "{}".format(experiment_data["name"]),
-                            "value": experiment_data
-                        }
-                        for experiment_data in experiment_data
-                    ],
+                    experiments{
+                      name
+                      environment
+                      resourceAccessTokenString
+                    } 
                 }
-            )
-            if choose_experiment == {}:
-                raise click.Abort()
-            experiment = choose_experiment["experiment"]
+                """
+    context.debug_msg(project_id)
+    project_data = context.api.get_project(project_id=project_id, fragment=fragment)
+    project_name = project_data["name"]
+    workspace_name = project_data["account"]["username"]
+    experiment_data = project_data["experiments"]
 
-        experiment_name = experiment["name"]
-        experiment_environment = experiment["environment"]
-        experiment_token = experiment["resourceAccessTokenString"]
+    click.secho("Workspace: {}".format(workspace_name), fg='blue')
+    click.secho("Project: {}<{}>".format(project_name, project_id), fg='blue')
 
-        click.secho("Experiment: {}".format(experiment_name), fg='blue')
-
-        """
-        index = -1
-        for i, x in enumerate(experiment_names):
-            if x == experiment_name:
-                index = i
-
-        # selecting corresponding experiment environment and token
-        experiment_environment = experiment_environments[index]
-        experiment_token = experiment_tokens[index]
-        """
-
-        link = f"{workspace_name}/{project_name}/{experiment_name}"
-        line4 = "# setup mlflow for this experiment\n"
-        line5 = "mlflow.set_tracking_uri('https://community.mlflow.deploif.ai')\n"
-        line6 = 'mlflow.set_experiment("{}")\n'.format(link)
-
-        if external or experiment_environment == Environment.EXTERNAL.value:
-
-            line1 = "# only if running an experiment outside deploifai (this must come before setting mlflow tracking uri and experiment)\n"
-            line2 = 'os.environ["MLFLOW_TRACKING_USERNAME"] = "{}" \n'.format(link)
-            line3 = 'os.environ["MLFLOW_TRACKING_PASSWORD"] = "{}" \n'.format(experiment_token)
-            required_code: str = line1 + line2 + line3 + line4 + line5 + line6
-
-        else:
-            required_code: str = line4 + line5 + line6
-
-        click.secho("The following code snippet needs to be added after importing mlflow", fg='green')
-        click.secho(required_code)
-        pyperclip.copy(required_code)
-        click.secho("It has also been copied to your clipboard", fg='green')
-    else:
-        click.secho("Unable to get project information (could not find .deploifai directory)", fg='red')
+    # selecting experiment name from the options
+    if len(experiment_data) == 0:
+        click.secho("No experiments exist for the project", fg='yellow')
         return
+    else:
+        choose_experiment = prompt(
+            {
+                "type": "list",
+                "name": "experiment",
+                "message": "Choose a experiment",
+                "choices": [
+                    {
+                        "name": "{}".format(experiment_data["name"]),
+                        "value": experiment_data
+                    }
+                    for experiment_data in experiment_data
+                ],
+            }
+        )
+        if choose_experiment == {}:
+            raise click.Abort()
+        experiment = choose_experiment["experiment"]
+
+    experiment_name = experiment["name"]
+    experiment_environment = experiment["environment"]
+    experiment_token = experiment["resourceAccessTokenString"]
+
+    click.secho("Experiment: {}".format(experiment_name), fg='blue')
+
+    link = f"{workspace_name}/{project_name}/{experiment_name}"
+    line4 = "# setup mlflow for this experiment\n"
+    line5 = "mlflow.set_tracking_uri('https://community.mlflow.deploif.ai')\n"
+    line6 = 'mlflow.set_experiment("{}")\n'.format(link)
+
+    if external or experiment_environment == Environment.EXTERNAL.value:
+
+        line1 = "# only if running an experiment outside deploifai (this must come before setting mlflow tracking uri and experiment)\n"
+        line2 = 'os.environ["MLFLOW_TRACKING_USERNAME"] = "{}" \n'.format(link)
+        line3 = 'os.environ["MLFLOW_TRACKING_PASSWORD"] = "{}" \n'.format(experiment_token)
+        required_code: str = line1 + line2 + line3 + line4 + line5 + line6
+
+    else:
+        required_code: str = line4 + line5 + line6
+
+    click.secho("The following code snippet needs to be added after importing mlflow", fg='green')
+    click.secho(required_code)
+    pyperclip.copy(required_code)
+    click.secho("It has also been copied to your clipboard", fg='green')
