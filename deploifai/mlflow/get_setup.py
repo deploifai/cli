6f13d8f8c07703cedbdf5df551297570d82213d4
KSChauhan2 <kunalsinghchauhan@gmail.com>
2022-07-07 15:00:27 +0800

    recommended changes
    changed experiment interpretation method and added enumeration for environment

--- deploifai/mlflow/get_setup.py
+++ deploifai/mlflow/get_setup.py
@@ -1,9 +1,16 @@
 import click
 import pyperclip
+from enum import Enum
 
 from deploifai.api import DeploifaiAPIError
 from deploifai.context import pass_deploifai_context_obj, DeploifaiContextObj
 from PyInquirer import prompt
+
+
+class Environment(Enum):
+    # Experiment Environment, DEPLOIFAI means usage of a managed runner, EXTERNAL means lack of a managed runner
+    DEPLOIFAI = 1
+    EXTERNAL = 2
 
 
 @click.command('get-setup')
@@ -38,19 +45,20 @@ def get_setup(context: DeploifaiContextObj, external):
         project_name = project_data["name"]
         workspace_name = project_data["account"]["username"]
         experiment_data = project_data["experiments"]
+
+        """
         experiment_names = [experiment["name"] for experiment in experiment_data]
         experiment_environments = [experiment["environment"] for experiment in experiment_data]
         experiment_tokens = [experiment["resourceAccessTokenString"] for experiment in experiment_data]
+        """
 
         click.secho("Workspace: {}".format(workspace_name), fg='blue')
         click.secho("Project: {}<{}>".format(project_name, project_id), fg='blue')
 
         # selecting experiment name from the options
-        if len(experiment_names) == 0:
+        if len(experiment_data) == 0:
             click.secho("No experiments exist for the project", fg='yellow')
             return
-        # elif len(experiment_names) == 1:
-        # experiment_name = experiment_names[0]
         else:
             choose_experiment = prompt(
                 {
@@ -58,16 +66,24 @@ def get_setup(context: DeploifaiContextObj, external):
                     "name": "experiment",
                     "message": "Choose a experiment",
                     "choices": [
-                        exp for exp in experiment_names
+                        {
+                            "name": "{}".format(experiment_data["name"]),
+                            "value": experiment_data
+                        }
                     ],
                 }
             )
             if choose_experiment == {}:
                 raise click.Abort()
-            experiment_name = choose_experiment["experiment"]
+            experiment = choose_experiment["experiment"]
+
+        experiment_name = experiment["name"]
+        experiment_environment = experiment["environment"]
+        experiment_token = experiment["resourceAccessTokenString"]
 
         click.secho("Experiment: {}".format(experiment_name), fg='blue')
 
+        """
         index = -1
         for i, x in enumerate(experiment_names):
             if x == experiment_name:
@@ -76,20 +92,22 @@ def get_setup(context: DeploifaiContextObj, external):
         # selecting corresponding experiment environment and token
         experiment_environment = experiment_environments[index]
         experiment_token = experiment_tokens[index]
+        """
 
         link = f"{workspace_name}/{project_name}/{experiment_name}"
         line4 = "# setup mlflow for this experiment\n"
         line5 = "mlflow.set_tracking_uri('https://community.mlflow.deploif.ai')\n"
         line6 = 'mlflow.set_experiment("{}")\n'.format(link)
-        if external or experiment_environment == "EXTERNAL":
+
+        if external or experiment_environment == Environment.EXTERNAL.name:
 
             line1 = "# only if running an experiment outside deploifai (this must come before setting mlflow tracking uri and experiment)\n"
             line2 = 'os.environ["MLFLOW_TRACKING_USERNAME"] = "{}" \n'.format(link)
             line3 = 'os.environ["MLFLOW_TRACKING_PASSWORD"] = "{}" \n'.format(experiment_token)
-            required_code: str = line1+line2+line3+line4+line5+line6
+            required_code: str = line1 + line2 + line3 + line4 + line5 + line6
 
         else:
-            required_code: str = line4+line5+line6
+            required_code: str = line4 + line5 + line6
 
         click.secho("The following code snippet needs to be added after importing mlflow", fg='green')
         click.secho(required_code)
