159c987175bc32bfbd506ccee69c954e71f443c6
KSChauhan2 <kunalsinghchauhan@gmail.com>
2022-07-26 15:59:19 +0800

    added help text to auth status

--- deploifai/auth/status.py
+++ deploifai/auth/status.py
@@ -6,10 +6,15 @@ from deploifai.context import (
     DeploifaiContextObj,
 )
 
+
 @click.command()
 @is_authenticated
 @pass_deploifai_context_obj
 def status(context: DeploifaiContextObj):
+    """
+        Check login status
+    """
+
     click.secho("You are logged in", fg="green")
 
     workspace = context.global_config["WORKSPACE"]["username"]
