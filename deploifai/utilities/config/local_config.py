46304251fa7b2d553dcc0eabad5a306b217ae677
KSChauhan2 <kunalsinghchauhan@gmail.com>
2022-07-26 16:21:55 +0800

    refactored local_config.py

--- deploifai/utilities/config/local_config.py
+++ deploifai/utilities/config/local_config.py
@@ -130,11 +130,21 @@ def add_data_storage_config(data_storage_id: str, config: configparser.ConfigPar
     :param config: The current config
     :return:
     """
-    if "id" in config["DATASET"] and len(config["DATASET"]["id"]) > 0:
+    if (
+            config is not None
+            and "DATASET" in config
+            and "id" in config["DATASET"]
+            and len(config["DATASET"]["id"]) != 0
+    ):
         raise DeploifaiDataAlreadyInitialisedError(
-            "This project already has a data storage attached to it."
+            "This project already has a dataset attached to it."
         )
 
-    config["DATASET"] = {"id": data_storage_id}
+    elif (
+            config is not None
+            and "DATASET" in config
+    ):
+
+        config["DATASET"] = {"id": data_storage_id}
 
     save_config_file(config)
