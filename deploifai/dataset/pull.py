108eba3cdcb47e248c4baaab4cd83313ca68deb6
KSChauhan2 <kunalsinghchauhan@gmail.com>
2022-08-02 16:27:06 +0800

    adding dataset pull command, editing gitignore

---
+++ deploifai/dataset/pull.py
@@ -0,0 +1,21 @@
+import click
+
+from deploifai.context import DeploifaiContextObj, pass_deploifai_context_obj, is_authenticated, dataset_found
+from deploifai.utilities.config.dataset_config import read_config_file
+from deploifai.core.data_storage import DataStorage
+
+
+@click.command()
+@pass_deploifai_context_obj
+@is_authenticated
+@dataset_found
+def pull(context: DeploifaiContextObj):
+    """
+    Download the dataset from the cloud to the current directory
+    """
+
+    api = context.api
+    dataset_id = context.dataset_config["DATASET"]["id"]
+
+    datastorage_handler = DataStorage(api, dataset_id)
+    datastorage_handler.pull()
