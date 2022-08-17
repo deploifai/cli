0dc00012178fa05349d16a4c59210ec0af5dff4f
KSChauhan2 <kunalsinghchauhan@gmail.com>
2022-08-17 14:52:37 +0800

    fixed provided information for dataset info

--- deploifai/dataset/info.py
+++ deploifai/dataset/info.py
@@ -32,49 +32,18 @@ def info(context: DeploifaiContextObj):
 
     click.secho("Workspace Name: {}".format(workspace_name), fg="blue")
     click.secho("Project Name: {}".format(project_name), fg="blue")
-    click.secho("Deploifai Storage ID: {}".format(dataset_id), fg="blue")
+    click.secho("Deploifai Dataset Name: {}".format(storage_details["name"]), fg="blue")
     click.secho("Dataset Status: {}".format(storage_details["status"]))
 
     url = get_dataset_route(workspace_name, project_name, dataset_id)
     click.secho(
-        "\nAccess the dataset on deploifai through: \n {}\n".format(url),
+        "\nAccess the dataset on deploifai through: \n{}\n".format(url),
         underline=True,
         fg="green",
     )
 
-    if cloud_provider == "AWS":
-        click.secho("AWS Storage Account", bold=True, fg="green", underline=True)
-        click.secho("Container Name: {}".format(container_name), fg="green")
-        click.secho(
-            "You could use the AWS CLI to list all the blobs in your container on AWS:",
-            underline=True,
-        )
+    click.secho("Container name to find the dataset on the cloud datastorage: {}".format(container_name), fg="green")
 
-        click.secho("aws s3 sync . s3://{}".format(container_name), fg="blue")
-
-    elif cloud_provider == "AZURE":
+    if cloud_provider == "AZURE":
         storage_account_name = storage_details["cloudProviderYodaConfig"]["azureConfig"]["storageAccount"]
-
-        click.secho("Azure Storage Account", bold=True, fg="blue", underline=True)
-        click.secho("Storage Account Name: {}".format(storage_account_name), fg="green")
-        click.secho("Container Name: {}".format(container_name), fg="green")
-        click.secho(
-            "You could use the Azure CLI to list all the blobs in your container on Azure:",
-            underline=True,
-        )
-        click.secho(
-            "az storage blob list -c {container_name} --account-name {storage_account_name}".format(
-                storage_account_name=storage_account_name, container_name=container_name
-            ),
-            fg="blue",
-        )
-
-    elif cloud_provider == "GCP":
-        click.secho("GCP Storage Account", bold=True, fg="green", underline=True)
-        click.secho("Container Name: {}".format(container_name), fg="green")
-        click.secho(
-            "You could use the GCP CLI to list all the blobs in your container on GCP:",
-            underline=True,
-        )
-
-        click.secho("gsutil -m rsync -r . gs://{}".format(container_name), fg="blue")
+        click.secho("Storage account name for Azure access: {}".format(storage_account_name), fg="green")
