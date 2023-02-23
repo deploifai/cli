05297099a49362e3ebd46a2a18ffbbe0f0290164
Jalik Yaw <70761798+yawjalik@users.noreply.github.com>
2023-02-23 23:59:11 +0800

    use local cloud profile credentials for aws when creating cloud profile (#61)
    * Use local aws credentials to create IAM user and secondary credentials
    
    * Attach more policies and handle errors
    
    * Added AWS profile selection prompt, removed manually entered credentials and removed AdministratorAccess
    
    * Cleanup and handle some mouse click errors
    
    * Changed error message for missing AWS credentials

--- deploifai/cloud_profile/create.py
+++ deploifai/cloud_profile/create.py
@@ -1,4 +1,6 @@
 import click
+import boto3
+from botocore.exceptions import ClientError
 from PyInquirer import prompt
 from deploifai.utilities.cloud_profile import Provider
 from deploifai.context import (
@@ -11,9 +13,15 @@ from deploifai.api import DeploifaiAPIError
 
 @click.command()
 @click.argument("name")
+@click.option(
+    "-p",
+    "--provider",
+    type=click.Choice([p.value for p in Provider]),
+    help="Provider for the new cloud profile"
+)
 @pass_deploifai_context_obj
 @is_authenticated
-def create(context: DeploifaiContextObj, name: str):
+def create(context: DeploifaiContextObj, name: str, provider: str):
     """
     Create a new cloud profile
     """
@@ -33,31 +41,91 @@ def create(context: DeploifaiContextObj, name: str):
         click.echo(click.style("Cloud profile name taken. Existing names: ", fg="red") + ' '.join(existing_names))
         raise click.Abort()
 
-    provider = prompt(
-        {
-            "type": "list",
-            "name": "provider",
-            "message": "Choose a provider for the new cloud profile",
-            "choices": [{"name": provider.value, "value": provider} for provider in Provider]
-        }
-    )["provider"]
+    if not provider:
+        try:
+            provider = prompt(
+                {
+                    "type": "list",
+                    "name": "provider",
+                    "message": "Choose a provider for the new cloud profile",
+                    "choices": [{"name": p.value, "value": p} for p in Provider]
+                }
+            )["provider"]
+        except KeyError:
+            click.secho('Mouse click detected', fg="red")
+            raise click.Abort()
+    else:
+        provider = Provider(provider)   # Cast to Provider enum
 
     cloud_credentials = {}
     if provider == Provider.AWS:
-        cloud_credentials["awsAccessKey"] = prompt(
-            {
-                "type": "input",
-                "name": "awsAccessKey",
-                "message": "AWS Access Key ID (We'll keep these secured and encrypted)",
-            }
-        )["awsAccessKey"]
-        cloud_credentials["awsSecretAccessKey"] = prompt(
-            {
-                "type": "input",
-                "name": "awsSecretAccessKey",
-                "message": "AWS Secret Access Key (We'll keep these secured and encrypted)",
-            }
-        )["awsSecretAccessKey"]
+        s = boto3.session.Session()
+        profiles = s.available_profiles
+        profile = 'default'
+        if not profiles:
+            click.secho('AWS credentials not found. Please set it up with "aws configure", or go to '
+                        'https://docs.deploif.ai/cloud-services/connect-your-account/aws to learn how to create AWS '
+                        'credentials for Deploifai on the dashboard.', fg="red")
+            raise click.Abort()
+        if len(profiles) > 1:
+            try:
+                profile = prompt(
+                    {
+                        "type": "list",
+                        "name": "profile",
+                        "message": "Choose an AWS profile to use",
+                        "choices": [{"name": p, "value": p} for p in profiles]
+                    }
+                )["profile"]
+                s = boto3.session.Session(profile_name=profile)
+            except KeyError:
+                click.secho('Mouse click detected', fg="red")
+                raise click.Abort()
+
+        local_creds = s.get_credentials()
+        iam = boto3.client('iam', aws_access_key_id=local_creds.access_key, aws_secret_access_key=local_creds.secret_key)
+
+        # Log in attempt - use an arbitrary service
+        try:
+            iam.list_users()
+        except ClientError as err:
+            if err.response['Error']['Code'] == 'InvalidClientTokenId':
+                click.secho(f"Invalid credentials for profile '{profile}'", fg="red")
+            else:
+                click.secho(err, fg="red")
+            raise click.Abort()
+        except Exception as err:
+            click.secho(err, fg="red")
+            raise click.Abort()
+
+        # Attempt to create user
+        try:
+            iam.create_user(Path="/", UserName=name)
+            click.echo(f"Created IAM user '{name}'")
+        except ClientError as err:
+            if err.response['Error']['Code'] == 'EntityAlreadyExists':
+                click.secho(f"IAM user '{name}' already exists", fg="red")
+            else:
+                click.secho(err, fg="red")
+            raise click.Abort()
+        except Exception as err:
+            click.secho(err, fg="red")
+            raise click.Abort()
+
+        try:
+            # Attach policies
+            iam.attach_user_policy(UserName=name, PolicyArn="arn:aws:iam::aws:policy/PowerUserAccess")
+            iam.attach_user_policy(UserName=name, PolicyArn="arn:aws:iam::aws:policy/IAMFullAccess")
+            click.echo("Attached policies")
+
+            # Create access key
+            access_key = iam.create_access_key(UserName=name)['AccessKey']
+            click.echo("Created access keys")
+            cloud_credentials["awsAccessKey"] = access_key['AccessKeyId']
+            cloud_credentials["awsSecretAccessKey"] = access_key['SecretAccessKey']
+        except Exception as err:
+            click.secho(err, fg="red")
+            raise click.Abort()
     elif provider == Provider.AZURE:
         cloud_credentials["azureSubscriptionId"] = prompt(
             {
